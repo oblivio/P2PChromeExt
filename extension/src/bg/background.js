cd24c59df285390ba38baeb0450c6a8e0ced8e70
OBLIVIO LLC <obliviollc@rfvs-MacBook-Air.local>
2021-12-28 00:24:12 -0500

    make the code even simpler. we dont need a lot of the other stuff

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -16,27 +16,12 @@
             "*://*/*" //get it all
         ]
     };
-    chrome.webRequest.onBeforeRequest.addListener((details) => {
-        const { tabId, requestId } = details;
-        if (!tabStorage.hasOwnProperty(tabId)) {
-            return;
-        }
-        tabStorage[tabId].requests[requestId] = {
-            requestId: requestId,
-            url: details.url,
-            startTime: details.timeStamp,
-            status: 'pending'
-        };
-        console.log(tabStorage[tabId].requests[requestId]);
-    }, networkFilters);
     chrome.webRequest.onCompleted.addListener((details) => {
         const { tabId, requestId } = details;
         if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
             return;
         }
-
         const request = tabStorage[tabId].requests[requestId];
-
         Object.assign(request, {
             endTime: details.timeStamp,
             requestDuration: details.timeStamp - request.startTime,
@@ -46,35 +31,4 @@
         chrome.storage.local.set({'history': JSON.stringify(tabStorage)})
     }, networkFilters);
 
-    chrome.webRequest.onErrorOccurred.addListener((details)=> {
-        const { tabId, requestId } = details;
-        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
-            return;
-        }
-
-        const request = tabStorage[tabId].requests[requestId];
-        Object.assign(request, {
-            endTime: details.timeStamp,           
-            status: 'error',
-        });
-        console.log(tabStorage[tabId].requests[requestId]);
-    }, networkFilters);
-
-    chrome.tabs.onActivated.addListener((tab) => {
-        const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
-        if (!tabStorage.hasOwnProperty(tabId)) {
-            tabStorage[tabId] = {
-                id: tabId,
-                requests: {},
-                registerTime: new Date().getTime()
-            };
-        }
-    });
-    chrome.tabs.onRemoved.addListener((tab) => {
-        const tabId = tab.tabId;
-        if (!tabStorage.hasOwnProperty(tabId)) {
-            return;
-        }
-        tabStorage[tabId] = null;
-    });
 }());
